02c802bfcb78fba277936ff1d807b8ced47d7b8d
Anton <antoniom@mail.ru>
2022-03-27 20:00:31 +0300

    27.03 №024 Практика

--- script.js
+++ script.js
@@ -1,73 +1,93 @@
 
-let numberOfFilms;
-
-function start() {
-  numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?');
-
-  while(numberOfFilms == null || numberOfFilms == '' || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?');
-  }
-
-}
-start();
+// let numberOfFilms;
 
 let personalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actors: {},
-genres: [],
-private: false,
-};
+  count: 0,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false,
+  start() {
+    // Здесь ищем не numberOfFilms  а Count, let numberOfFilms - уже не нужна
+    this.count = +prompt('Сколько фильмов Вы уже посмотрели?');
+    while(this.count == null || this.count == '' || isNaN(this.count)) {
+      this.count = +prompt('Сколько фильмов Вы уже посмотрели?');
+    }
+    console.log( this.count);
+  },
+  rememberFilms() {
+    for(let i = 0; i<2; i++) {
+      let movieName = prompt('Один из последних просмотренных фильмов?');
+      let rateMovie = +prompt('На сколько вы оценивает его?');
+    if(movieName != null && movieName != '' && rateMovie  != null && rateMovie != '' && movieName.length <15) {
+      this.movies[movieName] = rateMovie;
+    } else {
+      i--;
+    }
+    }
+  },
+  detectPersonalLevel() {
+    if(this.count <10) {
+      console.log('Просмотрено меньше 10 кино');
+    } else if (this.count >10 && this.count <30) {
+      console.log(`Просмотрено от 10 до 30 кино - а именно ${this.count}`); 
+    }
+      else if (this.count >30) {
+    console.log('Вы киноман!');
+    } else {
+      console.log('Ошибка');
+    }
+  },
 
+  writeGenres() {
+ // let num = 1;
+ for(let i=1; i<4; i++) {
+  this.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
 
-function rememberFilms() {
-  //Задаем два раза вопросы урок 14
-for(let i = 0; i<2; i++) {
-  let movieName = prompt('Один из последних просмотренных фильмов?');
-  let rateMovie = +prompt('На сколько вы оцениете его?');
-if(movieName != null && movieName != '' && rateMovie  != null && rateMovie != '' && movieName.length <15) {
-  personalMovieDB.movies[movieName] = rateMovie;
-} else {
-  i--;
-}
-}
-}
-// rememberFilms();
-
-function detectPersonalLevel() {
- if(personalMovieDB.count <10) {
-  console.log('Просмотрено меньше 10 кино');
-} else if (personalMovieDB.count >10 && personalMovieDB.count <30) {
-  console.log(`Просмотрено от 10 до 30 кино - а именно ${numberOfFilms}`); 
-}
-  else if (personalMovieDB.count >30) {
-console.log('Вы киноман!');
-} else {
-  console.log('Ошибка');
-}
-}
-// detectPersonalLevel();
-
-function showMyDB(private) {
-  if(private == false) {
-    console.log(personalMovieDB);
-  } else {
-    // если true - то не показываем
-    console.log('Объект закрыт для показа');
+  if(this.genres == null || this.genres == '') {
+    i--;
   }
-  }
-// showMyDB(personalMovieDB.private);
-
-function writeGenres() {
-  // let num = 1;
-  for(let i=1; i<4; i++) {
-    //можно через push
-    // personalMovieDB.genres.push(prompt(`Ваш любимый жанр под номером ${i}`));
-    // или просто через индекс [i-1]
-    personalMovieDB.genres[i-1] = prompt(`Ваш любимый жанр под номером ${i}`);
-   }
+//   // Если например юзер должен ввести фильмы за один раз через запятую то применяем метод split 
+// let gen = prompt(`Вашb любимые фильмы через запятую }`);
+// this.genres = gen.split(',');
+  // перевести строку в toLowerCase() - чтобы сортировка была верной
   
-}
-writeGenres();
+   
+  }
+    this.genres.forEach((item, index) => {
+    console.log(`Любимый жанр №${index +1} это ${ item}` );
+  });
+  // console.log(personalMovieDB.genres);
+ },
+   toggleVisibleDB() {
+     // если стоит в private false меняем на true и наоборот
+   this.private ? this.private = false : this.private = true; 
+   
+  },
+  showMyDB() {
+    if(this.private == false) {
+      console.log(personalMovieDB);
+    } else {
+      // если true - то не показываем
+      console.log('Объект закрыт для показа');
+    }
+  },
 
-console.log(personalMovieDB);
+  closeMovieDB() {
+   // this.private = true; //меняем разрешение на показ
+  }
+
+  };
+
+// personalMovieDB.start();
+// personalMovieDB.rememberFilms();
+// personalMovieDB.detectPersonalLevel();
+
+personalMovieDB.writeGenres();
+
+// personalMovieDB.closeMovieDB(); // закрываем для показа базу
+// personalMovieDB.toggleVisibleDB();
+// personalMovieDB.showMyDB();
+
+
+
